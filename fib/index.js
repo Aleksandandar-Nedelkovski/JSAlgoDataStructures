88272142480999eee2515f2f83b7672088e1d5d7
Aleksandandar-Nedelkovski <aleksn.dev@gmail.com>
2022-04-08 12:39:03 -0500

    queue and weave problems

--- fib/index.js
+++ fib/index.js
@@ -21,14 +21,14 @@ function memoize(fn) {
   };
 }
 
-function slowFib(n) {
+function fib(n) {
     if (n < 2) {
       return n
     }
     return fib(n - 1) + fib(n - 2)
   }
 
-const fib = memoize(slowFib);
+fib = memoize(fib);
 
 module.exports = fib;
 
