89b1783d0e7279aea6eef60a0d5d2a033bdf8ed5
Aleksandandar-Nedelkovski <aleksn.dev@gmail.com>
2022-04-08 21:32:09 -0500

    Node and LinkedList class constructor and insertFirst method

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,20 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+}
 
 module.exports = { Node, LinkedList };
